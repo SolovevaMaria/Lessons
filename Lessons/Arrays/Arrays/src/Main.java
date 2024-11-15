b1a580b16c23c97dfbe5a495ae3ea710ef3c21e3
SolovevaMaria <msoloveva606@gmail.com>
2024-11-15 22:48:45 +0300

    Homework

---
+++ Lessons/Arrays/Arrays/src/Main.java
@@ -0,0 +1,139 @@
+import java.util.Random;
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+// 1) Создайте массив из 10 целых чисел и заполните его числами от 1 до 10. Выведите массив на экран.
+//        int []arr = new int []{1,2,3,4,5,6,7,8,9,10};
+//        for (int i = 0; i < arr.length; i++) {
+//            System.out.print(arr[i] + " ");
+//        }
+
+//2) Найдите сумму элементов массива. Создайте массив из 5 элементов и вычислите сумму всех чисел в массиве.
+//       int []arr = new int[] {10,20,30,40,50};
+//        int sum = 0;
+//        for (int i = 0; i < arr.length; i++) {
+//               sum=sum+arr[i];
+//        }
+//        System.out.println(sum);
+
+// 3) Найдите максимальный элемент массива. Создайте массив из 10 целых чисел и найдите максимальное значение.
+// int arr[] = new int []{1,2,3,4,5,6,7,8,9,10};
+// int max = arr[0];
+//        for (int i = 0; i < arr.length; i++) {
+//            if (arr[i]>max);
+//            max = arr[i];
+//        }
+//        System.out.println(max);
+
+// 4) Найдите минимальный элемент массива. Создайте массив из 10 целых чисел и найдите минимальное значение.
+// int arr[] = new int []{1,2,3,4,5,6,7,8,9,10};
+// int min = arr[0];
+// for (int i = 0; i < arr.length; i++) {
+//   if (arr[i] <= min)
+//      min = arr[i];
+//        }
+// System.out.println(min);
+
+// 5) Посчитайте количество чётных чисел в массиве.
+// int arr[] = new int []{1,2,3,4,5,6,7,8,9,10};
+// int x = 0;
+// for (int i = 0; i < arr.length; i++) {
+//   if (arr[i]%2==0)
+//      x++;
+//        }
+// System.out.println(x);
+
+// 6) Посчитайте количество нечётных чисел в массиве.
+//int arr[] = new int []{1,2,3,4,5,6,7,8,9,10};
+//int x = 0;
+//for (int i = 0; i < arr.length; i++) {
+//if (arr[i]%2!=0)
+//    x++;
+//}
+//System.out.println(x);
+
+// 7) Посчитайте среднее арифметическое всех элементов массива.
+//int arr[] = new int []{1,2,3,4,5};
+//int x = 0;
+//int sum = 0;
+//for (int i = 0; i < arr.length; i++) {
+// sum  = sum + arr[i];
+// x = sum/ arr.length;
+//}
+//System.out.println(x);
+
+// 8) Выведите все элементы массива в обратном порядке.
+//int []arr = new int []{1,2,3,4,5,6,7,8,9,10};
+//for (int i = 9; i < arr.length; i--) {
+//System.out.print(arr[i] + " ");
+//}
+
+// 9) Заполните массив случайными числами от 1 до 100 и выведите его на экран.
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.print("enter arr size ");
+//        int size = scanner.nextInt();
+//        Random random = new Random();
+//
+//        int []arr = new int [size];
+//        for (int i = 0; i < arr.length; i++) {
+//                  arr[i]=random.nextInt(101);
+//        }
+//        for (int i = 0; i < arr.length; i++) {
+//            System.out.print(arr[i]+" ");
+//        }
+
+// 10)Поменяйте местами первый и последний элементы массива
+// int[] arr = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+// int a = arr[0];  
+// arr[0] = arr[9];
+// arr[9] = a;
+// for (int i = 0; i < arr.length; i++) {
+//  System.out.print(arr[i] + " ");
+//}
+//
+//
+
+// 11)Найдите разницу между максимальным и минимальным элементами массива.
+//int []arr = new int []{1,2,3,4,5,6,7,8,9,10};
+//int max = arr[0];
+//int min = arr[0];
+//int x = 0;
+//for (int i = 0; i < arr.length; i++) {
+//    if (arr[i] > max) ;
+//    max = arr[i];
+//}
+//for (int i = 0; i < arr.length; i++) {
+//if (arr[i]>max);
+//max = arr[i];
+//}
+//x = max + min;
+//System.out.println(x);
+
+// 12)Посчитайте количество элементов, больше среднего арифметического.
+//int arr[] = new int []{1,2,3,4,5};
+//int x = 0;
+//int sum = 0;
+//int a = 0;
+//for (int i = 0; i < arr.length; i++) {
+//    sum = sum + arr[i];
+//    x = sum / arr.length;
+//}
+//    for (int i = 0; i < arr.length; i++) {
+//        if (arr[i]>x)
+//            a++;
+//    }
+//
+//System.out.println(a);
+
+// 13)Создайте массив и заполните его квадратами чисел от 1 до 10.
+//        int arr[] = new int[]{1,2,3,4,5,6,7,8,9,10};
+//        for (int i = 0; i < arr.length; i++) {
+//           arr[i]=arr[i]*arr[i];
+//            System.out.print(arr[i] + " ");
+//        }
+
+    }
+}
--- Lessons/Whele.Do while/While/src/Main.java
+++ Lessons/Whele.Do while/While/src/Main.java
@@ -2,6 +2,123 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+// While
+//Прямоугольник
+//
+// int x = 0;
+// while (x<=10){
+//     System.out.println("* * * * *");
+//     x++;
+// }
 
+
+// Фигура 2
+
+//  int x=0;
+//  int y=0;
+//     int z = 0;
+//  while (y<=6 && x<=6){
+//      System.out.println("* ");
+//       y++;
+//      System.out.println(" ");
+//      x++;
+// }
+
+
+
+
+      
+
+//Фигура 3
+// Треугольник 1
+//      int x =8;
+//      int y = x;
+//        while (y > 0){
+//          for (int i = 0; i < y; i++) {
+//                   System.out.print("* ");
+//               }
+//               System.out.println();
+//               y--;
+//               x--;
+//           }
+
+
+//Треугольник   2
+//        int x = 8;
+//        int y = 1;
+//        while (x>0){
+//            for (int i = 0; i < y; i++) {
+//                System.out.print("* ");
+//            }
+//            System.out.println();
+//            y++;
+//            x--;
+//        }
+
+// Треугольник 3
+//int x = 8;       // строки в треугольнике
+//int y = 1; // начальное количество *
+//   while (x>0) {   //пока количество строк больше 0
+//    for (int i = 0; i < y; i++) {           //* в строке
+//                System.out.print("* ");
+//
+//            }
+//
+//            System.out.println();
+//            y++;                       // увеличение *
+//            x--;                       // уменьшение строк
+//
+//        }
+
+
+//Ромб
+
+
+
+// Do while
+//Прямоугольник
+//int x = 0;
+// do  {
+//     System.out.println("* * * * *");
+//     x++;
+// }
+//  while (x<=10);
+
+// Фигура 2
+// int x=0;
+// int y=0;
+// int z = 0;
+//       do   {
+//    System.out.println("* ");
+//       y++;
+//    System.out.println(" ");
+//    x++;
+// }
+//  while (y<=6 && x<=6);
+
+//Фигура 3
+// Треугольник 1
+//Треугольник   2
+// Треугольник 3
+//Ромб
+
+//Только границы фигур
+//Прямоугольник
+        for (int i = 0; i <= 7; i++) {
+             if (i==0){
+                 System.out.println("* * * * *");
+             }
+             if (i == 7)   {
+                 System.out.println("* * * * *");
+                 break;
+             }
+            System.out.println("*       *");
+        }
+// Фигура 2
+//Фигура 3
+// Треугольник 1
+//Треугольник   2
+// Треугольник 3
+//Ромб
     }
 }
